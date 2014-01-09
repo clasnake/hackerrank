45726621a7e6d06a346edd4f4e9ad1e759bc9b59
clasnake <classical.zhu@gmail.com>
2014-01-09 17:04:20 +0800

    Add the solution to find_merge_node of a linked list.

---
+++ find_merge_node.cpp
@@ -0,0 +1,64 @@
+#include<iostream>
+#include<cstdio>
+#include<cstdlib>
+using namespace std;
+struct Node
+{
+	int data;
+	Node* next;
+};/*
+   Find merge point of two linked lists
+   Node is defined as
+   struct Node
+   {
+       int data;
+       Node* next;
+   }
+*/
+int FindMergeNode(Node *headA, Node *headB)
+{
+    if (headA->next == NULL) {
+        return headA->data;
+    }
+    if (headB->next == NULL) {
+        return headB->data;
+    }
+    Node* p = headA;
+    while (p->next != NULL) {
+        p = p->next;
+    }
+    p->next = headA;
+    Node* slow = headB;
+    Node* fast = headB;
+    while (fast != NULL && fast->next != NULL) {
+        slow = slow->next;
+        fast = fast->next->next;
+        if (slow == fast) {
+            break;
+        }
+    }
+    slow = headB;
+    while (slow != fast) {
+        slow = slow->next;
+        fast = fast->next;
+    }
+    return slow->data;
+}int main()
+{
+	Node *A, *B, *C, *D,*E,*F,*G;
+	A = new Node();	B= new Node();  C= new Node(); D = new Node(); E = new Node(); F= new Node();G = new Node();
+	A->data = 2; B->data = 4; C->data = 3; D->data = 5; E->data = 7; F->data = 6;G->data = 11;
+
+	// case 1 = 
+	A->next = B; B->next = C; C->next = D; D->next = E; E->next = NULL;
+	F->next = G; G->next = C;
+	cout<<FindMergeNode(A,F)<<"\n";
+	//case 2.
+	A->next = B; B->next = C; C->next = E;  E->next = NULL;
+	F->next = G; G->next = D;D->next = C;
+	cout<<FindMergeNode(A,F)<<"\n";
+	//case 3:
+	A->next = B; B->next = E; E->next = NULL;
+	F->next = G; G->next = D;D->next = C; C->next = E;
+	cout<<FindMergeNode(A,F)<<"\n";
+}
